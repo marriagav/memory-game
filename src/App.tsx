dcfeede3dfa63e91745584948e36e3b84ab736a2
Miguel Arriaga Velasco <marriagav3@gmail.com>
2023-04-04 20:36:07 -0600

    good

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const numberOfPokemon = 10;
+  const pokemonCards: JSX.Element[] = [];
+  for (let i = 1; i <= numberOfPokemon; i++) {
+    pokemonCards.push(
+      <PokemonCard
+        pokemonNumber={i}
+        className={"pokemonCard"}
+        cardId={uuidv4()}
+      ></PokemonCard>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
