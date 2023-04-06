f56aea86af9c56a56f9833621b9946970836f60f
Miguel Arriaga Velasco <marriagav3@gmail.com>
2023-04-06 09:19:40 -0600

    game finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,13 @@ import PokemonCard from "./components/PokemonCard";
 import DisplayScore from "./components/DisplayScore";
 import { v4 as uuidv4 } from "uuid";
 import { useState, useEffect } from "react";
-import { useCardsList } from "./hooks/useCardList";
 
 function App() {
   const [highScore, setHighScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
   const [listOfVisited, setListOfVisited] = useState<string[]>([]);
   const [pokemonCards, setPokemonCards] = useState<JSX.Element[]>([]);
-  const numberOfPokemon = 12;
-  // const { addPokemonToList, currentScore, highScore } = useCardsList();
-  // const [po]
-  // const pokemonCards: JSX.Element[] = [];
-  // console.log()
+  const numberOfPokemon = 30;
 
   function addPokemonToList(pokemonId: string) {
     if (listOfVisited.includes(pokemonId)) {
@@ -23,31 +18,27 @@ function App() {
       setListOfVisited([]);
     } else {
       setListOfVisited(listOfVisited.concat(pokemonId));
-      console.log(pokemonId);
-      console.log(listOfVisited);
-      console.log(currentScore);
       setCurrentScore(currentScore + 1);
-      setHighScore(Math.max(highScore, currentScore));
+      setHighScore(Math.max(highScore, currentScore + 1));
     }
   }
 
   useEffect(() => {
-    console.log("building");
     const pokemonCardsTemp: JSX.Element[] = [];
     for (let i = 1; i <= numberOfPokemon; i++) {
-      const pokemonId: string = uuidv4();
+      // const pokemonId: string = uuidv4();
       pokemonCardsTemp.push(
         <PokemonCard
           pokemonNumber={i}
           className={"pokemonCard"}
-          pokemonId={pokemonId}
+          pokemonId={i.toString()}
           onClick={addPokemonToList}
-          key={pokemonId}
+          key={i.toString()}
         ></PokemonCard>
       );
     }
-    setPokemonCards(pokemonCardsTemp);
-  }, []);
+    setPokemonCards(pokemonCardsTemp.sort((a, b) => 0.5 - Math.random()));
+  }, [currentScore]);
 
   return (
     <div className="App">
@@ -55,20 +46,22 @@ function App() {
         <h1>Memory Game</h1>
       </header>
       <main>
-        <div className="scoresContainer">
-          <DisplayScore
-            className="scoreDisplay"
-            scoreName="Current Score"
-            scoreNumber={currentScore}
-            id="currentScore"
-          ></DisplayScore>
-          <DisplayScore
-            className="scoreDisplay"
-            scoreName="Best Score"
-            scoreNumber={highScore}
-            id="bestScore"
-          ></DisplayScore>
-        </div>
+        {
+          <div className="scoresContainer">
+            <DisplayScore
+              className="scoreDisplay"
+              scoreName="Current Score"
+              scoreNumber={currentScore}
+              id="currentScore"
+            ></DisplayScore>
+            <DisplayScore
+              className="scoreDisplay"
+              scoreName="Best Score"
+              scoreNumber={highScore}
+              id="bestScore"
+            ></DisplayScore>
+          </div>
+        }
         <div className="cardsContainer">{pokemonCards}</div>
       </main>
     </div>
